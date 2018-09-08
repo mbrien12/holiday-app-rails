8eed4e670a32b7485761c6ba9c61a7ae0ea0ee94
Michelle Brien <michelle.brien12@gmail.com>
2018-09-08 15:07:23 +0100

    User model added and basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :holidays
+  resources :users
+  
   root 'holidays#index'
+  get 'signup' => 'users#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180907164551) do
+ActiveRecord::Schema.define(version: 20180908132237) do
 
   create_table "holidays", force: :cascade do |t|
     t.string "location"
     t.text "description"
     t.integer "flight_price"
     t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "avatar"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

f0a9512668cc265e52ae014d4cecce2ab34d95de
Michelle Brien <michelle.brien12@gmail.com>
2018-09-30 18:59:02 +0100

    added voter validation and error message

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,11 @@
 class VotesController < ApplicationController
+  
   def create
     @vote = Vote.new(holiday_id: params[:holiday_id], voter_id: params[:voter_id])
     if @vote.save
       redirect_to holidays_path, :flash => { :success => "You successfully voted" }
     else
-      redirect_to holidays_path, :flash => { :error => "Vote didn't save" }
+      redirect_to holidays_path, :flash => { :error => @vote.errors.values.flatten.join("\n") }
     end
   end 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,5 @@
 class Vote < ApplicationRecord
   belongs_to :voter, :class_name => "User"
   belongs_to :holiday
+  validates_uniqueness_of :voter_id, :message => "Sorry, looks like you've already voted for this one."
 end
